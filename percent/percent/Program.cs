78062c2a2510bfe1543e88eb40ab2fdcfbf39aa0
Gaharit <Gaharit@gmail.com>
2017-02-25 09:01:08 +0800

    1

---
+++ percent/percent/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace percent
+{
+    class Program
+    {
+        static int months = 12, percent = 100;
+        public static double Calculate(string userInput)
+        {
+            string[] separator = new string[] {" "};
+
+            string[] info = userInput.Split(separator, System.StringSplitOptions.None);
+            double sum = double.Parse(info[0]);
+            double interestRate = double.Parse(info[1]) / months / percent;
+            int time = int.Parse(info[2]);
+            sum *= Math.Pow(1 + interestRate, time);
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            string userInput = Console.ReadLine();
+            Console.WriteLine(Calculate(userInput));
+            Console.ReadKey();
+        }
+    }
+}
